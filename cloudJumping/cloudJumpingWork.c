4e1f53d0fdd10ea1325c4ac9d37d363e0ba1873a
Becky828 <rmj.schiele@gmail.com>
2020-04-21 22:27:54 -0700

    Here is the latest version of cloud jumping.  It correctly completes all of the tests.  Any follow up version will be utilized to clean up the unused code.

--- cloudJumping/cloudJumpingWork.c
+++ cloudJumping/cloudJumpingWork.c
@@ -14,9 +14,11 @@ char* readline();
 int jumpingOnClouds(int c_count, int* c) {
 	int i = 0;
 	int jumps = 0;
+	int min = 0;
 	int end_point = 0;
-	int last_jump = 0;
-		int array_count;
+	int fir_pos_last_jump = 0;
+	int sec_pos_last_jump = 0;
+	int array_count;
 	int current_index = 0;
 	int test_index = 0;
 	int next_even_index = 0;
@@ -24,14 +26,21 @@ int jumpingOnClouds(int c_count, int* c) {
 	int next_odd_index = 0;
 	int next_next_odd_index = 0;
 	const char *type[c_count];
-	//	while(i < c_count){
-	end_point = --c_count; 
-	++c_count;
-last_jump = --end_point;
-++end_point;
+	//    while(i < c_count){
+	fir_pos_last_jump = c_count-=2;
+	sec_pos_last_jump = ++c_count;
+	end_point = c_count; 
+++c_count;
+min = 2;
+	printf("endpoint test: %d\n\n", end_point);
 	for(i = 0; i < c_count; i++){
-		//	for(i = 0; i < end_point; i++){
-		printf("i: %d\n", i);
+	if(c_count == min){
+
+++jumps;
+i = c_count;
+	}
+		//    for(i = 0; i < end_point; i++){
+		//	printf("i: %d\n", i);
 		/*
 		   current_index = i;
 		   if((current_index % 2) == 0){
@@ -42,7 +51,7 @@ last_jump = --end_point;
 		   current_index = i;
 		   printf("current index: %d next even index: %d\n\n", current_index, next_even_index);
 		   }
-		   if((++current_index) <= c_count){	
+		   if((++current_index) <= c_count){    
 		   next_odd_index = current_index;
 		   current_index = i;
 		   printf("current index: %d next odd index: %d\n\n", current_index, next_odd_index);
@@ -51,7 +60,7 @@ last_jump = --end_point;
 		   next_next_even_index = current_index;
 		   current_index = i;
 		   printf("current index: %d next next even index: %d\n\n", current_index, next_next_even_index);
-		   }	
+		   }    
 		   if((current_index+=3) <= c_count){
 		   next_next_odd_index = current_index;
 		   current_index = i;
@@ -67,7 +76,7 @@ last_jump = --end_point;
 		   current_index = i;
 		   printf("current index: %d next even index: %d\n", current_index, next_even_index);
 		   }
-		   if((current_index+=2) <= c_count){	
+		   if((current_index+=2) <= c_count){    
 		   next_odd_index = current_index;
 		   current_index = i;
 		   printf("current index: %d next odd index: %d\n", current_index, next_odd_index);
@@ -76,30 +85,30 @@ last_jump = --end_point;
 		   next_next_even_index = current_index;
 		   current_index = i;
 		   printf("current index: %d next next even index: %d\n", current_index, next_next_even_index);
-		   }	
+		   }    
 		   if((current_index+=4) <= c_count){
 		   next_next_odd_index = current_index;
 		   current_index = i;
 		   printf("current index: %d next next odd index: %d\n", current_index, next_next_odd_index);
 		   }
 		   }
-		//	i = current_index;
+		//    i = current_index;
 
 
 		if(((current_index % 2) == 0) && (c[current_index] != 1)){
 		printf("valid even index\n");
 		++jumps;
-		//	current_index =	++i;
+		//    current_index =    ++i;
 		//++i;
 		printf("ai: %d jumps: %d\n", i, jumps);
 
 		if(c[next_even_index] == 1){
-		//	++i;
-		//	i = current_index;
+		//    ++i;
+		//    i = current_index;
 		if(c[next_odd_index] != 1){
 		++jumps;
 		printf("bi: %d jumps: %d\n", i, jumps);
-		//	i = current_index;
+		//    i = current_index;
 
 
 		++i;
@@ -119,7 +128,7 @@ last_jump = --end_point;
 	}
 	if(((current_index % 2) != 0)){
 		printf("valid odd index\n");
-		//	++i;
+		//    ++i;
 	}
 
 	++i;
@@ -130,7 +139,7 @@ last_jump = --end_point;
 	if((type[i] == "even") && (c[i] == 0)){
 		if(i < (--c_count)){
 			++jumps;
-			//	array_count = c_count;
+			//    array_count = c_count;
 		}
 		++c_count;
 	}
@@ -139,7 +148,7 @@ last_jump = --end_point;
 		printf("next odd index: %d\n", next_odd_index);
 
 		if(c[++i] == 0){
-			if(i < (c_count-=3)){	
+			if(i < (c_count-=3)){    
 				//c_count+=3;
 				array_count = c_count;
 
@@ -163,156 +172,223 @@ last_jump = --end_point;
 
 		}
 		--i;
-		//	if(i < (c_count-4)){
+		//    if(i < (c_count-4)){
 
 		if(c[i+=3] == 0){
-			//	if(i < (c_count-1)){
+			//    if(i < (c_count-1)){
 			i-=3;
 			++jumps;
-			//	}
+			//    }
 		}
 		*/
-			//	}
+			//    }
 			//}
+	else{
 			current_index = i;
+		printf("a Jumped from current index: %d\n\n", current_index);
 		if((i % 2) == 0){
 			i = current_index;
-			printf("this i and endpoint: %d\t%d\n\n", i, end_point);
+			current_index+=2;
+			printf("a Jumped to current index: %d\n\n", current_index);
+			//	printf("this i and endpoint: %d\t%d\n\n", i, end_point);
 			if(current_index < c_count){
-				//	if(i < end_point){
-				//	if(current_index < end_point){
-				//	++c_count;
-
+				//    if(i < end_point){
+				//    if(current_index < end_point){
+				//    ++c_count;
+				//printf("\n\nJumped from current index: %d\n\n", current_index);
 				if(c[current_index] == 0){
-					//	if(c[current_index] == 0){
+					//    if(c[current_index] == 0){
 					printf("a jumps: %d\n\n",++jumps);
 					while(c[current_index] == 0){
-				i+=2;
-				current_index+=2;
-						if(current_index < last_jump){
-						++jumps;
-					}
-					//if(i <  end_point){
-					//	i+=2;
-					//	current_index+=2;
-			printf("a i: %d\tcurrent index: %d\n\n", i, current_index);
-						//	}
+						printf("\n\nc Jumped from current index: %d\n\n", current_index);
+						i+=2;
+						current_index+=2;
+						printf("d Jumped to current index: %d\n\n", current_index);
+						printf("first possible last jump: %d\tsecond possibe last jump: %d\n\n", fir_pos_last_jump, sec_pos_last_jump);
+						if(((current_index <= fir_pos_last_jump) || (current_index <= sec_pos_last_jump)|| (current_index <= c_count)) && (c[current_index] == 0)){
+							printf("b jumps: %d\n\n",
+									++jumps);
+							/*
+							   if(current_index == c_count){
+							   ++current_index;
+							   }
+							   }
+*/
+					}	
+						if(current_index == c_count){
+								++jumps;
+								++current_index;
+							}
+						//}
+						//if(current_index == c_count){
+						//     i-=3;
+						//}
+						/*	if((current_index > fir_pos_last_jump) || (current_index > sec_pos_last_jump)){
+						*/
+						if(current_index > c_count){
+
+
+							// ++jumps;
+							i = c_count;
+							break;
+						}
+						//if(i <  end_point){
+						//    i+=2;
+						//    current_index+=2;
+						/*
+						   printf("a i: %d\tcurrent index: %d\n\n", i, current_index);
+						   */
+						//    }
 						//++i;
 					}
-		/*
+					/*
+					   if(c[current_index] == 1){
+					   printf("while loop a stopped at current_index: %d\ti: %d\n\n", current_index, i);
+					   i-=3;
+					   }
+					   */    
+					}
+					//    }
+					/*
+					   printf("i test 1: %d\n\n", i);    
+					   printf("current index test 1: %d\n\n", current_index);
+					   printf("c[current_index] test 1: %d\n\n", c[current_index]);    
+					   */
+					//    if(c[i] == 1){
 					if(c[current_index] == 1){
-printf("while loop a stopped at current_index: %d\ti: %d\n\n", current_index, i);
-		i-=3;
-			}
-		*/	
-				}
-				//	}
-				printf("i test 1: %d\n\n", i);	
-				printf("current index test 1: %d\n\n", current_index);
-				printf("c[current_index] test 1: %d\n\n", c[current_index]);	
-				//	if(c[i] == 1){
-				if(c[current_index] == 1){
-					//	if((i % 2) == 1){
-					//	printf("b jumps: %d\n\n", ++jumps);
-					//	if(c[++i] == 0){
-					if(c[--current_index] == 0){
-						printf("b jumps: %d\n\n", ++jumps);
-						while((c[current_index]) != 1){
-					i+=2;
-					current_index+=2;
-							if(current_index < last_jump){
-							++jumps;
-						}
-					//	if(i < end_point){
-						//	i+=2;
-				//	current_index+=2;
-					printf("b i: %d\tcurrent index: %d\n\n", i, current_index);
-					//	}
-						}
-			/*	
-				if(c[current_index] == 1){
-printf("while loop b stopped at current_index: %d\ti: %d\n\n", current_index, i);
-		i-=3;	
-				}
-			*/
-				}
-					printf("hello current index %d\n\n", current_index);	
-					/*	if(((current_index+=3) < end_point) && c[current_index] == 0){
-						if(c[--current_index] == 0){
+						printf("\n\ne Jumped from current index: %d\n\n", current_index-=2);
+						i-=2;
+						//    if((i % 2) == 1){
+						//    printf("b jumps: %d\n\n", ++jumps);
+						//    if(c[++i] == 0){
+
+						//	if(c[--current_index] == 0){
+						//	printf("b jumps: %d\n\n", ++jumps);
+						printf("f Jumped to current index: %d\n\n", ++current_index);		
 						printf("c jumps: %d\n\n", ++jumps);
-					//	i = current_index;
+						++i;
+						while((c[current_index]) != 1){
+							printf("\n\ng Jumped from current index: %d\n\n", current_index);
+							i+=2;
+							current_index+=2;
+							printf("h Jumped to current index: %d\n\n", current_index);
+							if(((current_index <= fir_pos_last_jump) || (current_index <= sec_pos_last_jump)|| (current_index <= c_count)) && c[current_index] == 0){
+								printf("d jumps: %d\n\n", ++jumps);
+								/*
+								   if(current_index == end_point){
+								   ++current_index;
+								   }
+								   */
+							}
+								if(current_index == c_count){
+									++jumps;
+									++current_index;
+								}
+							
+							/*
+							   if((current_index > fir_pos_last_jump) || (current_index > sec_pos_last_jump)){
+							   */	
+							// ++jumps;
+							if(current_index > c_count){	
+								i = c_count;
+								break;
+							}
+							// if(current_index == c_count){
+							//   i-=3;
+							//}
+							//    if(i < end_point){
+							//    i+=2;
+							//    current_index+=2;
+							/*
+							   printf("b i: %d\tcurrent index: %d\n\n", i, current_index);
+							   */
+							//    }
+
+					}
+
+					if(c[current_index] == 1){
+						printf("while loop stopped at current_index: %d\ti: %d\n\n", current_index, i);
+						i-=2;    
+					}
+
+					//	}
+					//		printf("hello current index %d\n\n", current_index);    
+					/*    if(((current_index+=3) < end_point) && c[current_index] == 0){
+					      if(c[--current_index] == 0){
+					      printf("c jumps: %d\n\n", ++jumps);
+					//    i = current_index;
 					}
 					++i;
-					//	}*/
-					//	i = current_index;
-					//	++i;
-				}	
-				/*	
-					test_index = ++i;
-					printf("bi: %d\n\n", i);
+					//    }*/
+					//    i = current_index;
+					//    ++i;
+					}    
+					/*    
+					      test_index = ++i;
+					      printf("bi: %d\n\n", i);
+					      if(test_index < end_point){
+					//    ++c_count;
+					if(c[test_index] == 0){
+					printf("c jumps: %d\n\n", ++jumps);
+					}
+					}
+					test_index = i+=2;
+					printf("i test 2: %d\n\n", i);
 					if(test_index < end_point){
-				//	++c_count;
-				if(c[test_index] == 0){
-				printf("c jumps: %d\n\n", ++jumps);
-				}
-				}
-				test_index = i+=2;
-				printf("i test 2: %d\n\n", i);
-				if(test_index < end_point){
-				//	++c_count;
-				if(c[test_index] == 0){
-				printf("d jumps: %d\n\n", ++jumps);
-				}
-				if(c[test_index] == 1){
-				--test_index;
-				if(c[test_index] == 0){
-				if((test_index % 2) != 0){
-				printf("e jumps: %d\n\n", ++jumps);
-				}
-				printf("this test_index: %d\n\n", test_index);	
-				if((test_index% 2) == 0)
-				{
+					//    ++c_count;
+					if(c[test_index] == 0){
+					printf("d jumps: %d\n\n", ++jumps);
+					}
+					if(c[test_index] == 1){
+					--test_index;
+					if(c[test_index] == 0){
+					if((test_index % 2) != 0){
+					printf("e jumps: %d\n\n", ++jumps);
+					}
+					printf("this test_index: %d\n\n", test_index);    
+					if((test_index% 2) == 0)
+					{
 
-				i-=2;
-				}
+					i-=2;
+					}
 
-				}
-				}
+					}
+					}
 
-				}	
+					}    
 
-				}
-				*/
-				}
+					}
+					*/
+					}
 
-			}
-			printf("\nc count: %d\n\n", c_count);
-			}
-
-			//	printf("\nc count: %d\n\n", c_count);
-			/*
-			   for(int i = 0; i < c_count; i++){
-			   printf("%s\t", type[i]);
-			   }
-			   printf("\n");
-			   for(int i = 0; i < c_count; i++){
-			   printf("%d\t", c[i]);
-			   }
-			   */
-			//}
-			for(int i = 0; i < c_count; i++){
-				if((i%10) != 0){
-					printf("%d: %d\t", i, c[i]);
-				}
-				if((i%10) == 0){
-					printf("\n%d: %d\t", i, c[i]);
+					}
+					printf("\nc count: %d\n\n", c_count);
 				}
 			}
-			printf("\n\n");
-			//for(int i = 0; i < c_count; i++){
-			//	printf("%d\t", c[i]);
-			//}
-			return jumps;
+				//    printf("\nc count: %d\n\n", c_count);
+				/*
+				   for(int i = 0; i < c_count; i++){
+				   printf("%s\t", type[i]);
+				   }
+				   printf("\n");
+				   for(int i = 0; i < c_count; i++){
+				   printf("%d\t", c[i]);
+				   }
+				   */
+				//}
+				for(int i = 0; i < c_count; i++){
+					if((i%10) != 0){
+						printf("%d: %d\t", i, c[i]);
+					}
+					if((i%10) == 0){
+						printf("\n%d: %d\t", i, c[i]);
+					}
+				}
+				printf("\n\n");
+				//for(int i = 0; i < c_count; i++){
+				//    printf("%d\t", c[i]);
+				//}
+				return jumps;
 			}
 			int main()
 			{
@@ -328,31 +404,34 @@ printf("while loop b stopped at current_index: %d\ti: %d\n\n", current_index, i)
 				   char* s = readline();
 				   */
 
-			//		int n = 7;
-				//	int n = 6;
+				//	int n = 7;
+			//	int n = 6;
 				//int n =9;
 				//	int n = 10;
-			int n = 85;
+		//	int n = 85;
+		int n = 2;
 				int c_count = n;
+			int c[] = {0, 0};
 				//	int c[] = {0, 1, 0, 0, 0, 1, 0};
 				//	int c[] = {0, 0, 1, 0, 0, 0, 0};
-				//here		int c[] = {0, 0, 1, 0, 0, 1, 0};
+				// int c[] = {0, 0, 1, 0, 0, 1, 0};
 				//	int c[] = {0, 1, 0, 0, 1, 0, 0};
 				//int c[] = {0, 0, 1, 0, 1, 0, 0};
 				//		int c[] = {0, 0, 0, 1, 0, 1, 0};
 				//	int c[] = {0, 1, 0, 1, 0, 0, 0};
-				//	int c[] = {0, 0, 0, 1, 0};
+			//here		int c[] = {0, 0, 0, 1, 0, 0};
 				//	int c[] = {0, 1, 0, 0, 1, 0, 1, 0, 0};
 				//	int c[] = {0, 1, 0, 0, 1, 0, 0, 1, 0, 0};
 				//int c[] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0};
 				//int c[] = {0, 0, 0, 1, 0, 1, 0, 0, 1, 0};
-					
-				int c[] = {0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 
-					0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 
-					0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1,
-					0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0};
+			/*	
+				   int c[] = {0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 
+				   0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 
+				   0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1,
+				   0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0};
+			*/	
 
-					
+
 				int result = jumpingOnClouds(c_count, c);
 
 				//	fprintf(fptr, "%d\n", result);
